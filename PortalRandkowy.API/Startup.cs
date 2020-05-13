d0cbd3539c727f12934800725702b807da74aa8d
PW <przemek.wrobel@poczta.fm>
2020-05-13 15:45:26 +0200

    Dodany pierwszy model i  pakiet EF

--- PortalRandkowy.API/Startup.cs
+++ PortalRandkowy.API/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using PortalRandkowy.API.Data;
 
 namespace PortalRandkowy.API
 {
@@ -25,6 +20,7 @@ namespace PortalRandkowy.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(x => x.UseSqlite("ConectionString"));
             services.AddControllers();
         }
 
@@ -38,7 +34,6 @@ namespace PortalRandkowy.API
 
             app.UseHttpsRedirection();
 
-            app.UseRouting();
 
             app.UseAuthorization();
 
