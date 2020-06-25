6d4c7d81782b69e8dfcc71bbbaec63bb4011749d
PW <przemek.wrobel@poczta.fm>
2020-06-25 13:53:02 +0200

    Dodany interface IAuthRepository

--- PortalRandkowy.API/Data/DataContext.cs
+++ PortalRandkowy.API/Data/DataContext.cs
@@ -3,14 +3,15 @@ using PortalRandkowy.API.Models;
 
 namespace PortalRandkowy.API.Data
 {
-    public class DataContext :DbContext
-    { 
-        public DataContext(DbContextOptions<DataContext> options) :base(options) 
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
 
         }
 
         public DbSet<Value> Values { get; set; }
-        
+        public DbSet<User> Users { get; set; }
+
     }
 }
