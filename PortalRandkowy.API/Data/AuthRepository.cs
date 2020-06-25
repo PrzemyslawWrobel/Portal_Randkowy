66fe205c4da07119adf975c5d61eef67737b4e4d
PW <przemek.wrobel@poczta.fm>
2020-06-25 15:20:20 +0200

    Method Logowania

--- PortalRandkowy.API/Data/AuthRepository.cs
+++ PortalRandkowy.API/Data/AuthRepository.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using PortalRandkowy.API.Models;
 
 namespace PortalRandkowy.API.Data
@@ -15,9 +16,17 @@ namespace PortalRandkowy.API.Data
             _context = context;
 
         }
-        public Task<User> Login(string username, string password)
+        public async Task<User> Login(string username, string password)
         {
-            throw new System.NotImplementedException();
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Username == username);
+
+            if (user == nameof)
+                return;
+
+            if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+                return null;
+
+            return user;
         }
 
         public async Task<User> Register(User user, string password)
@@ -43,6 +52,23 @@ namespace PortalRandkowy.API.Data
         // Method private
 
         #region Method Private
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using(var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != passwordHash[i])
+                        return false;
+                }
+                return true;
+            }
+
+        }
+
         private void CreatePasswordHashSalt(string password, out byte[] passwordHash, out byte[] passwordSalt)
         {
             using(var hmac = new System.Security.Cryptography.HMACSHA512())
