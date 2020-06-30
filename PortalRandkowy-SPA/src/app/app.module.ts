17102414e6e54c40fd4e5e94f437e0c1d82eebcd
PW <przemek.wrobel@poczta.fm>
2020-06-30 15:09:01 +0200

    Formularz logowania, walidacja wrowadzanych danych

--- PortalRandkowy-SPA/src/app/app.module.ts
+++ PortalRandkowy-SPA/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { NavComponent } from './nav/nav.component';
+
 
 @NgModule({
    declarations: [
@@ -16,7 +18,8 @@ import { NavComponent } from './nav/nav.component';
    imports: [
       BrowserModule,
       HttpClientModule,
-      AppRoutingModule
+      AppRoutingModule,
+      FormsModule
    ],
    providers: [],
    bootstrap: [
