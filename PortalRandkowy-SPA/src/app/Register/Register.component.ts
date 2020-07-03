b8f57a87f53e03967c92cfc4712e626ea3fa0da5
PW <przemek.wrobel@poczta.fm>
2020-07-03 16:54:11 +0200

    warunkowe wyświetlanie dla rejestracj

--- PortalRandkowy-SPA/src/app/Register/Register.component.ts
+++ PortalRandkowy-SPA/src/app/Register/Register.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+
   selector: 'app-Register',
   templateUrl: './Register.component.html',
   styleUrls: ['./Register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  model: any = {};
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  register() {
+    console.log(this.model);
+  }
+
+  cancel() {
+    console.log('Anulowane');
+  }
 }
