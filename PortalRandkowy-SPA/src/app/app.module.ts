e445f3a168432300f343c90f12970453e4c9b980
PW <przemek.wrobel@poczta.fm>
2020-07-07 15:39:19 +0200

    Komunikacja komponent rodzic -> dziecko

--- PortalRandkowy-SPA/src/app/app.module.ts
+++ PortalRandkowy-SPA/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
 import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './Home/Home.component';
@@ -15,7 +14,6 @@ import { RegisterComponent } from './Register/Register.component';
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent,
       NavComponent,
       HomeComponent,
       RegisterComponent
