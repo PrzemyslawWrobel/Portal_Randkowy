bfa97476bab4bfd233a0f5c30296b6d43608aea1
PW <przemek.wrobel@poczta.fm>
2020-07-22 10:35:53 +0200

    ustawiamy linki n awigacyjne

--- PortalRandkowy.SPA/src/app/routes.ts
+++ PortalRandkowy.SPA/src/app/routes.ts
@@ -5,9 +5,9 @@ import { LikesComponent } from './likes/likes.component';
 import { MessegesComponent } from './messeges/messeges.component';
 
 export const appRoutes: Routes = [
-    { path: ' home', component: HomeComponent },
-    { path: ' użytkownicy ', component: UsersListComponent },
-    { path: ' polubienia', component: LikesComponent },
-    { path: ' wiadomości', component: MessegesComponent },
-    { path: ' **', redirectTo: ' home ', pathMatch: 'full' }
+    { path: 'home', component: HomeComponent },
+    { path: 'uzytkownicy', component: UsersListComponent },
+    { path: 'polubienia', component: LikesComponent },
+    { path: 'wiadomosci', component: MessegesComponent },
+    { path: '**', redirectTo: 'home', pathMatch: 'full' },
 ];
