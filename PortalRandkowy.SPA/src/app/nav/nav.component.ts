1db3df138e86ccda7f9fcd5f5009ab50f193b7e7
PW <przemek.wrobel@poczta.fm>
2020-07-22 13:32:35 +0200

    przekierowanie do wybranej podstrony w aplikacji

--- PortalRandkowy.SPA/src/app/nav/nav.component.ts
+++ PortalRandkowy.SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 declare let alertify: any;
 
 @Component({
@@ -12,7 +13,7 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor(public authService: AuthService, private alertifyService: AlertifyService) { }
+  constructor(public authService: AuthService, private alertifyService: AlertifyService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -22,6 +23,8 @@ export class NavComponent implements OnInit {
       this.alertifyService.success('Zalogowałeś sie do aplikacji');
     }, error => {
       this.alertifyService.error('Wystąpił błąd logowania');
+    }, () => {
+      this.router.navigate(['/uzytkownicy']);
     });
   }
 
@@ -32,6 +35,7 @@ export class NavComponent implements OnInit {
   logout() {
     localStorage.removeItem('token');
     this.alertifyService.message('Zostałeś wylogowany');
+    this.router.navigate(['/home']);
   }
 
 }
